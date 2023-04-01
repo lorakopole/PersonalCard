8c3ffdb810c7122762da5cf45ac27095a03dc058
Karol Jagiełło <karoljagiello.mobile@gmail.com>
2023-04-02 00:13:14 +0200

    Dodano testowe elementy ekranu początkowego

--- PersonalCard/ViewController.swift
+++ PersonalCard/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BackgroundSwitch: UISwitch!
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainLabel.textColor = .white
+        BackgroundSwitch.isOn = false
+        loginTextField.isHidden = true
+        loginButton.isHidden = true
+        passwordTextField.isHidden = true
     }
 
 
+    @IBAction func SwitchChanged(_ sender: UISwitch) {
+        
+        if BackgroundSwitch.isOn
+        {
+            mainView.backgroundColor = UIColor.black
+        }
+        else
+        {
+            mainView.backgroundColor = UIColor.white
+        }
+        loginTextField.isHidden = false
+        passwordTextField.isHidden = false
+        loginButton.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500), execute: {
+            self.BackgroundSwitch.isHidden = true
+        })
+        
+    }
 }
 
